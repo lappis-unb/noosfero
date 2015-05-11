23a7e816dbb1f4ad281f2510d51429e28ee0ca73
DylanGuedes <djmgguedes@gmail.com>
2015-05-11 15:24:57 -0300

    init of the ContentTree plugin
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>
    Signed-off-by: Filipe Ribeiro <firibeiro77@live.com>
    Signed-off-by: Álvaro Fernando <alvarofernandoms@gmail.com>

---
+++ plugins/content_tree/lib/content_tree_plugin.rb
@@ -0,0 +1,13 @@
+class ContentTreePlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    # FIXME
+    "ContentTreePlugin"
+  end
+
+  def self.plugin_description
+    # FIXME
+    _("A plugin that does this and that.")
+  end
+
+end
