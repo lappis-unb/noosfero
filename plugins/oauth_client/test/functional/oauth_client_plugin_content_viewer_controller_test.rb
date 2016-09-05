4eb779a613711c962c4f0bd55f85f4919a8b2fed
David Carlos <ddavidcarlos1392@gmail.com>
2016-09-05 03:11:27 -0300

    Test hide spread button to external user

--- plugins/oauth_client/test/functional/oauth_client_plugin_content_viewer_controller_test.rb
+++ plugins/oauth_client/test/functional/oauth_client_plugin_content_viewer_controller_test.rb
@@ -2,23 +2,31 @@ require 'test_helper'
 
 class ContentViewerControllerTest < ActionController::TestCase
 
-	def setup
-		@community = Community.create(identifier: "mycommunity", name: "My Community")
-		@community.blocks.each {|i| i.destroy}
-		@community.boxes[0].blocks << ProfileInfoBlock.new
-		@community.save!
-		@article = TinyMceArticle.create!(:profile => @community, :name => 'An article whithin the community')
-		@article.published = true
-		@article.save!
-	end
+  def setup
+    @community = Community.create(identifier: "mycommunity", name: "My Community")
+    @external_person = ExternalPerson.create(identifier: "ze1",
+                                             name: "ze",
+                                             source: "zeenvironment.org",
+                                             email: "ze@mail.com",
+                                             created_at: Date.yesterday)
+    @profile = create_user('foo').person
+  end
 
-	should 'show allowed user actions for normal user' do
-		@profile = create_user('foo').person
-		login_as('foo')
-		@article.stubs(:allow_spread?).with(@profile.user).returns(true)
-		get :view_page, profile: @community.identifier, page: @article.slug
-		assert_tag tag: "a", :descendant => {:tag => "span" , :child => /Spread this/ }
-	end
+  should 'show allowed user actions for normal user' do
+    @community = Community.create(:name => 'testcomm')
+    @community.articles.create!(:name => 'myarticle', :body => 'test article', :published => true)
+    login_as('foo')
+    get :view_page, :profile => @community.identifier, :page => ['myarticle']
+    assert_tag tag: "a", attributes: {:class => "modal-toggle button with-text icon-spread"}
+  end
+
+  should 'show hide user actions for external user' do
+    session[:external] = @external_person.id
+    @community.articles.create!(:name => 'myarticle', :body => 'test article', :published => true)
+    get :view_page, :profile => @community.identifier, :page => ['myarticle']
+    assert_no_tag tag: "a", attributes: {:class => "modal-toggle button with-text icon-spread"}
+  end
+
 
 end
 
